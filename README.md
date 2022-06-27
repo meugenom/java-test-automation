e97a25cecb2eb3496d7e0ac0b871efb3fd266587
Eugen Morozov <meugenom@gmail.com>
2022-06-27 20:17:56 +0200

    added allure reports environment

--- README.md
+++ README.md
@@ -2,12 +2,36 @@
 
 These testing cases are as learning of Selenium web-driver. The observable website is [https://demoqa.com](https://demoqa.com).  
 You can use this Java framework as a boilerplate to build your own. I'll try to add new features soon.
+ 
+### What's installed:
+1. Browser ChromeDriver:
+
+```
+brew install ChromeDriver
+```
+
+See versions of ChromeDriver and Web Browser Chrome. In our case, it's 103.0.5060.53 for both.
+For macOS Need to allow executed ChromeDriver in System>Security>Privacy.
+
+2. Install allure:
+
+```
+brew install allure
+```
 
 #### How to run:
+1. Start our tests:
 
 ```
 mvn clean test
 ```
+
+2. See all results in allure test reports:
+
+```
+./allure.sh
+```
+
 
 📌 *notation keys:*:
  - [`✍🏻`] - in progress;
--- src/test/java/com/meugenom/OpenBrowserTest.java
+++ src/test/java/com/meugenom/OpenBrowserTest.java
@@ -2,7 +2,6 @@ package com.meugenom;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,7 +10,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Link;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import io.qameta.allure.Step;
 
+@Epic("Allure examples")
+@Feature("Junit 5 support")
 public class OpenBrowserTest {
 	
 	final Logger logger = LoggerFactory.getLogger(OpenBrowserTest.class);
@@ -19,25 +28,35 @@ public class OpenBrowserTest {
     private WebDriver driver;
 
     @BeforeEach
+	@Description("Initiate New Chrome Driver")
+	@Feature("Feature - New Chrome Driver")
 	public void setup() {
         driver = new ChromeDriver();
     }
 
-    @AfterEach
+	@AfterEach
+	@Description("Quit Chrome Driver")
+	@Feature("Feature - Quit Chrome Driver")
     public void teardown() {
         driver.quit();
     }
 
-    @Test
-	@DisplayName("Open Browser Test")
+	@Test
+    @Description("Checking the title of the loaded page.")
+	@Feature("Feature - Daoing website search")
+	@Link("https://demoqa.com")
+	@Severity(SeverityLevel.NORMAL)
+	@Story("Base support for bdd annotations")
+    @Story("Advanced support for bdd annotations")
+	@Step("Step - Launch Browser")
 	public void testOpenBrowser() throws InterruptedException {
 
-    
 		// Exercise
         String sutUrl = "https://demoqa.com/automation-practice-form";
         driver.get(sutUrl);
         String title = driver.getTitle();
         logger.info("The title of {} is {}", sutUrl, title);
+        System.out.println("Page Loaded");
 
         // Verify
         assertThat(title).isEqualTo("ToolsQA");
